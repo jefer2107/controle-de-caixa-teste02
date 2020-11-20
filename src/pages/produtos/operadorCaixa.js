fc9a451d0ff581c5aab2a9522665f763dc4d6857
Jefferson Dias <54519758+jefer2107@users.noreply.github.com>
2020-11-19 23:53:39 -0300

    criando a função operador de caixa-1.2

--- src/pages/produtos/operadorCaixa.js
+++ src/pages/produtos/operadorCaixa.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react"
 import "./operadorCaixa.css"
-import { returnItemByCode } from "../../services/productService"
+import { cancelItem, productRegistration, removeItem, returnItemByCode } from "../../services/productService"
 
 export default function OperadorCaixa(){
     const [model, setModel] = useState({codigo:""})
     const [item, setItem] = useState([])
+    const [message, setMessage] = useState()
+    let valorTotal = 0
 
     const changeModel = ({target})=>{
         setModel((state)=>{
@@ -13,46 +15,81 @@ export default function OperadorCaixa(){
     }
 
     const addItem = (itens)=>{
-        if(!item.some(e=>e.codigo === itens.codigo))
+        if(!item.some(e=>e.codigo === itens.codigo)){
             setItem((state)=>{
                 return [...state, itens]
             })
+        }
+           
     }
 
     useEffect(()=>{
         if(model.codigo.length === 6){
             returnItemByCode(model.codigo).then((x)=>{
                 addItem(x)
+                removeItem(model.codigo)
                 setModel({codigo:""})
+            }).catch(()=>{
+                setMessage("Ítem não encontrado")
             })
         }
     }, [model.codigo])
+
+    const delet = (code)=>{
+        cancelItem(code).then(()=>{
+            setItem((state)=>{
+                const newItem = [...state]
+                const verifyItem = item.findIndex(e=> e.codigo === code)
+                newItem.splice(verifyItem, 1)
+                return newItem
+            })
+        })
+    }
 
     return(
         <div className="container-fluid operador-caixa">
             <form className="container py-4">
                 <h3 className="text-center py-4 font-weight-bold">Operador de caixa</h3>
                 <input onChange={changeModel} className="form-control" value={model.codigo} type="text" name="codigo" placeholder="Digite o código de barras" />
+                <div className="text-center font-weight-bold">
+                    <span className="text-primary my-2"> {message} </span>
+                </div>
             </form>
-            <table className="container">
+            <table className="container tabela-1">
                 <thead className="text-center font-weight-bold">
                     <tr>
+                        <td>CÓD. BARRAS</td>
                         <td>NOME</td>
                         <td>TIPO</td>
-                        <td>VALOR</td>
+                        <td>VALOR UNIT.</td>
+                        <td>TOTAL</td>
+                        <td>CANCELAMENTO</td>
                     </tr>
                 </thead>
                 <tbody>
                     {item.map((x)=>{
                         return(
                             <tr>
+                                <td> {x.codigo} </td>
                                 <td> {x.nome} </td>
                                 <td> {x.tipo} </td>
                                 <td> {x.valor} </td>
+                                <td> {valorTotal = valorTotal + x.valor} </td>
+                                <td>
+                                    <button onClick={()=> delet(x.codigo)} type="button" className="btn btn-danger btn-block">Cancelar</button>
+                                </td>
                             </tr>
                         )
                     })}
                 </tbody>
+            </table>
+            <table className="tabela-2 container">
+                <thead>
+                    <tr>
+                        <td className="td-1 font-weight-bold">TOTAL A PAGAR</td>
+                        <td className="td-2 text-center"> {valorTotal} </td>
+                    </tr>
+                </thead>
             </table>
         </div>
     )
--- src/services/productService.js
+++ src/services/productService.js
@@ -4,7 +4,21 @@ const product = [
         nome:"arroz",
         tipo:"alimento",
         codigo:"111111",
-        valor:"50"
+        valor:50
+    },
+    {
+        id:"1",
+        nome:"detergente",
+        tipo:"limpeza",
+        codigo:"222222",
+        valor:20
+    },
+    {
+        id:"2",
+        nome:"biscoito",
+        tipo:"alimento",
+        codigo:"333333",
+        valor:5
     }
 ]
 
@@ -22,6 +36,15 @@ export const productRegistration = (item)=>{
     return Promise.resolve()
 }
 
+export const cancelItem = (item)=>{
+    const itemToBeCanceled = product.find(e=>e.codigo === item)
+
+    if(itemToBeCanceled) return Promise.reject()
+    
+    product.push(itemToBeCanceled)
+    return Promise.resolve()
+}
+
 export const returnItem = ()=>{
     return Promise.resolve(product)
 }
@@ -36,7 +59,7 @@ export const returnItemByCode = (codigo)=>{
     const codeToBeReturn = product.find(e=>e.codigo === codigo)
 
     if(!codeToBeReturn) 
-        return Promise.reject("Ítem nao encontrado")
+        return Promise.reject()
     else
         return Promise.resolve(codeToBeReturn)
 }
